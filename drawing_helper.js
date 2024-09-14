bf7850752d828a82c2449766945908bbfc000b4a
Robert Denomme <Robert.Denomme@optitex.com>
2024-09-14 12:55:14 -0400

    Start drawing!

--- drawing_helper.js
+++ drawing_helper.js
@@ -1,19 +1,37 @@
 
 class DrawingHelper{
-    constructor(context)
+    constructor(context, height_pixels, cm_to_pixels)
     {
         this.context = context
+        this.cm_to_pixels = cm_to_pixels
+        this.height_pixels = height_pixels
+    }
+
+
+    worldToPixelSpace(p)
+    {
+        let ret = new Vec2(0, 0);
+        ret.x = this.cm_to_pixels * p.x
+        ret.y = this.height_pixels - this.cm_to_pixels * p.y
+        return ret
+    }
+
+    drawLinePixels(p_0_pixel, p_1_pixel)
+    {
+        this.context.save();
+        this.context.beginPath();
+        this.context.moveTo(p_0_pixel.x, p_0_pixel.y);
+        this.context.lineTo(p_1_pixel.x, p_1_pixel.y);
+        this.context.stroke();
+        this.context.restore();
+        // console.log(p_0_pixel.x, p_0_pixel.y)
+        // console.log(p_1_pixel.x, p_1_pixel.y)
+        
     }
 
     drawLine(p_0, p_1)
     {
-        this.context.save();
-        this.context.beginPath();
-        this.context.moveTo(p_0.x, p_0.y);
-        this.context.lineTo(p_1.x, p_1.y);
-        this.context.stroke();
-        this.context.restore();
+        this.drawLinePixels(this.worldToPixelSpace(p_0), this.worldToPixelSpace(p_1))
     }
-
 
 }
